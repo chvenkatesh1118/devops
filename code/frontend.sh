fb11178fdd58b703fb5d1fa6e228e1ad75a8fa04
Chanti <chvenkatesh.1118@gmail.com>
2022-01-31 12:23:30 +0530

    31-01

--- code/frontend.sh
+++ code/frontend.sh
@@ -3,12 +3,3 @@
  sudo yum install nginx -y
  sudo systemctl enable nginx
  sudo systemctl start nginx
- sudo curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
- sudo cd /usr/share/nginx/html
- sudo rm -rf *
- sudo unzip /tmp/frontend.zip
- sudo mv frontend-main/* .
- sudo mv static/* .
- sudo rm -rf frontend-master static README.md
- sudo mv localhost.conf /etc/nginx/default.d/roboshop.conf
- sudo systemctl restart nginx
