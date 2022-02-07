4bba3d98e08a69b4dbc567fd23985c4fdb7b4090
Chanti <chvenkatesh.1118@gmail.com>
2022-02-07 20:08:39 +0530

    07-02

--- code/sg.tf
+++ code/sg.tf
@@ -1,27 +1,15 @@
 
-resource "aws_security_group" "myall" {
-  name        = "myall"
-  description = "Allow all"
-     vpc_id   = aws_vpc.vpc0.id
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      =  ["0.0.0.0/0"]
-    ipv6_cidr_blocks = []
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
+resource "aws_vpc" "main" {
+  cidr_block           = var.public_vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
   tags = {
-    Name = "myall"
+    Name = var.env
   }
+}
+
+resource "aws_vpc_ipv4_cidr_block_association" "private_cidr" {
+  count      = length(var.private_vpc_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.private_vpc_cidr, count.index)
 }
