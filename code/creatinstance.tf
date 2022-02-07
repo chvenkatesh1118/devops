5f56164491c52af36b47c2513af289b0725cf06a
Chanti <chvenkatesh.1118@gmail.com>
2022-02-07 16:01:29 +0530

    07-02

--- code/creatinstance.tf
+++ code/creatinstance.tf
@@ -7,32 +7,6 @@ resource "aws_vpc" "vpc0" {
   }
 }
 #########################################################################################################
-resource "aws_security_group" "myall" {
-  name        = "myall"
-  description = "Allow all"
-  vpc_id      = aws_vpc.vpc0.id
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "ssh"
-    cidr_blocks      = [aws_vpc.vpc0.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.vpc0.ipv6_cidr_block]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "myall"
-  }
-}
 
 ##########################################################################################################
 resource "aws_subnet" "sub1" {
