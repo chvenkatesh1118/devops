be7c7756e382571c63f498529ce43f5487c50e75
Chanti <chvenkatesh.1118@gmail.com>
2022-02-07 15:09:38 +0530

    07-02

--- code/creatinstance.tf
+++ code/creatinstance.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "frontend" {
-  ami_id         =  "ami-0d997c5f64a74852c"
+  ami        =  "ami-0d997c5f64a74852c"
   instance_type = "t2.micro"
     subnet_id   = "subnet-0f42b250c3cf1d75c"
-    vpc_id      = "vpc-0bf57a4f41c708816"
+    vpc    = "vpc-0bf57a4f41c708816"
     vpc_security_group_ids = "sg-03439c9546a230549"
 
   tags = {
