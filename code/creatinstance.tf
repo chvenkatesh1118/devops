ea320137561b20d8fa59db97cfc219e9eb1d25f7
Chanti <chvenkatesh.1118@gmail.com>
2022-02-07 15:14:37 +0530

    07-02

--- code/creatinstance.tf
+++ code/creatinstance.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "frontend" {
   instance_type = "t2.micro"
     subnet_id   = "subnet-0f42b250c3cf1d75c"
 
-    vpc_security_group = "sg-03439c9546a230549"
+
 
   tags = {
     Name = "frontend"
